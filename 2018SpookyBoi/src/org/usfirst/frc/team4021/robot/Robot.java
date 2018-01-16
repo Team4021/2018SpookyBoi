c95e6009de178daedd49307316e306f4060283d3
User <User@172.22.100.248>
2018-01-16 16:17:31 -0600

    VictorSP's

--- 2018SpookyBoi/src/org/usfirst/frc/team4021/robot/Robot.java
+++ 2018SpookyBoi/src/org/usfirst/frc/team4021/robot/Robot.java
@@ -5,8 +5,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.*;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.*;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,9 +25,16 @@ public class Robot extends IterativeRobot {
 	Joystick Xbox = new Joystick(1);
 	double Tide;
 	double Pods;
-	double Knuckles;
-	RobotDrive EvanDrive;
-	WPI_TalonSRX FrontLeft, RearLeft, FrontRight, RearRight;
+		VictorSP FrontLeft = new VictorSP(0);
+		VictorSP RearLeft = new VictorSP(3);
+	   SpeedControllerGroup Drive_Left = new SpeedControllerGroup(FrontLeft, RearLeft);
+
+	   VictorSP FrontRight = new VictorSP(1);
+	   VictorSP RearRight = new VictorSP(2);
+	   SpeedControllerGroup Drive_Right = new SpeedControllerGroup(FrontRight, RearRight);
+
+	   DifferentialDrive MainDrive = new DifferentialDrive(Drive_Left, Drive_Right);
+
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -36,11 +45,6 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
 		SmartDashboard.putData("Auto choices", chooser);
-		FrontLeft = new WPI_TalonSRX(1);
-		FrontRight = new WPI_TalonSRX(2);
-		RearLeft = new WPI_TalonSRX(3);
-		RearRight = new WPI_TalonSRX(4);
-		EvanDrive = new RobotDrive(FrontLeft, RearLeft, FrontRight, RearRight);
 	}
 
 	/**
@@ -83,8 +87,9 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-    	EvanDrive.arcadeDrive(Xbox);
-    		Timer.delay(0.01);
+		Tide = Xbox.getRawAxis(1);
+		Pods = Xbox.getRawAxis(0);
+		MainDrive.arcadeDrive(Tide, Pods);
 		
 	}
 
